32ebc2e94d675a69a9041e00d6a0068febdec9cb
Danny Shen <shenfive@gmail.com>
2019-12-14 14:09:47 +0800

    create User UI

---
+++ hellofirebase02/CreateAccountViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateAccountViewController.swift
+//  hellofirebase02
+//
+//  Created by 申潤五 on 2019/12/14.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password1: UITextField!
+    @IBOutlet weak var password2: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func send(_ sender: Any) {
+    }
+}
