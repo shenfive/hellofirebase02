046c2028eae1231877cd2c93f6c8a41805ef7f6f
Danny Shen <shenfive@gmail.com>
2019-12-14 13:45:45 +0800

    Initial Commit

---
+++ hellofirebase02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hellofirebase02
+//
+//  Created by 申潤五 on 2019/12/14.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
